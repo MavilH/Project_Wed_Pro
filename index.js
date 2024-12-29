e7ef345eec76f131a8ab445538040b9a98197c76
lehoangduy23092005@gmail.com <lehoangduy23092005@gmail.com>
2024-12-29 23:24:08 +0700

    chinh xua link anh trong file index.html

--- index.js
+++ index.js
@@ -13,20 +13,45 @@ const init = () => {
 };
 document.addEventListener("DOMContentLoaded", init());
 
+//hàm sẽ sẽ hiện các thẻ có nội dung giống với ô search
+const searchFunction = (inputValue) => {
+  if (inputValue.trim()) {
+    let parentParts = document.querySelectorAll(".part-parent-a");
+    let childParts = document.querySelectorAll(".part-child-a");
+
+    let filteredParentParts = [...parentParts].filter((parentPart) =>
+      parentPart.innerText.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    let filteredChileParts = [...childParts].filter((childPart) =>
+      childPart.innerText.toLowerCase().includes(inputValue.toLowerCase())
+    );
+
+    //cho ẩn hết menu
+    document.querySelectorAll(".part-parent").forEach((parentPart) => {
+      parentPart.classList.remove("actived");
+    });
+    document.querySelectorAll(".part-child").forEach((childPart) => {
+      childPart.classList.remove("actived");
+    });
+
+    //hiển thị ParentPart
+    filteredParentParts.forEach((parentPart) => {
+      parentPart.parentNode.classList.add("actived");
+    });
+    filteredChileParts.forEach((childPart) => {
+      childPart.parentNode.parentNode.parentNode.classList.add("actived");
+    });
+  } else {
+    init();
+  }
+};
+
 document.querySelector("#filter").addEventListener("keyup", (event) => {
   let inputValue = event.target.value;
   searchFunction(inputValue);
 });
 
-document.querySelector("#btnSearch").addEventListener("click", (event) => {
+document.querySelector(".btn-search").addEventListener("click", (event) => {
   let inputValue = document.querySelector("#filter").value;
   searchFunction(inputValue);
-});
-
-const parts = document.querySelectorAll(".part-child-a");
-let a = [...parts];
-a.forEach((e) => {
-  if (e.innerHTML.includes("eee")) {
-    e.parentNode.parentNode.parentNode.classList.remove("actived");
-  }
 });
